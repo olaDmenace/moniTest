89b832ab9970ac5e41fef67dbf3462bdff399958
olaDmenace <olayinkafagbenro@gmail.com>
2024-04-19 13:48:53 +0100

    transactionsItems layout

--- src/components/transaction/TransactionItem.js
+++ src/components/transaction/TransactionItem.js
@@ -24,8 +24,8 @@ const TransactionItem = ({ amount, sender, spendType, sent }) => {
   return (
     <div className="flex font-bold justify-between items-center h-fit py-3 lg:py-1 md:py-2 even:bg-white odd:bg-slate-200 px-2">
       <span className="text-xs text-black/50">
-        <h3 className="text-black lg:text-sm">{senderName}</h3>
         {spendType}
+        <h3 className="text-black lg:text-sm">{senderName}</h3>
       </span>
       <h3
         className={sent ? "text-red-600" : "text-green-600"}
